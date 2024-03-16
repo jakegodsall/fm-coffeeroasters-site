a3bcfb2b655889344bfb68f347e4ee8a1f5a8948
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-16 19:28:10 +0100

    removed Image for background of home hero and use background images

--- src/app/(home)/components/Hero.jsx
+++ src/app/(home)/components/Hero.jsx
@@ -6,20 +6,8 @@ import Button from "@/app/components/UI/Button";
 
 export default function Hero() {
   return (
-    <section className="relative w-full overflow-hidden rounded-[1rem]">
+    <section className="sm:bg-home-hero-desktop relative w-full overflow-hidden rounded-[1rem] bg-home-hero-mobile bg-center bg-no-repeat">
       <div className="flex flex-col items-center px-[2rem] py-[10rem] text-center text-white sm:items-start">
-        <Image
-          src={heroBackgroundMobile}
-          alt="coffeepress"
-          fill
-          className="-z-10 sm:hidden"
-        />
-        <Image
-          src={heroBackgroundTablet}
-          alt="coffeepress"
-          fill
-          className="-z-10 hidden sm:inline"
-        />
         <div className="max-w-[40rem] sm:ml-[5.6rem] sm:text-left">
           <h1 className="mb-[2.4rem] font-fraunces text-[3.8rem] font-black leading-[4rem]">
             Great coffee made simple
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
           "linear-gradient(to bottom, white, white 60%, rgba(255, 255, 255, 0));",
         "text-linear-gradient":
           "linear-gradient(180deg, rgba(0, 0, 0, 40%) 0%, #FEFCF7 100%)",
+        "home-hero-mobile":
+          "url(/assets/home/mobile/image-hero-coffeepress.jpg)",
+        "home-hero-desktop":
+          "url(/assets/home/desktop/image-hero-coffeepress.jpg)",
         "about-hero-mobile":
           "url(/assets/about/mobile/image-hero-whitecup.jpg)",
         "about-hero-desktop":
