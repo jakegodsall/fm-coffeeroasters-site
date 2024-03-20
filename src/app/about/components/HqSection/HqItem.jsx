b7051eece05207717193dbbe1654f4ca60799fd7
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-20 08:26:19 +0100

    Added basic HqItem component

--- src/app/about/components/HqSection/HqItem.jsx
+++ src/app/about/components/HqSection/HqItem.jsx
@@ -1,3 +1,12 @@
-export default function HqItem() {
-  return <div></div>;
+export default function HqItem({ title, address, phone, icon }) {
+  return (
+    <div>
+      {icon}
+      <h3>{title}</h3>
+      {Object.values(address).map((line, idx) => (
+        <p key={idx}>{line}</p>
+      ))}
+      <p>{phone}</p>
+    </div>
+  );
 }
