dd7b4f36df84164d72d4b38afb160bdc763f38fe
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-27 05:58:52 +0000

    added tablet layout to Hero component of about page

--- src/app/about/components/Hero.jsx
+++ src/app/about/components/Hero.jsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
-    <section className="bg-about-hero-mobile flex w-full flex-col items-center bg-cover bg-center bg-no-repeat pt-[11rem] text-center">
-      <div className="mb-[8.7rem] px-[2.4rem]">
+    <section className="flex w-full flex-col items-center bg-about-hero-mobile bg-cover bg-center bg-no-repeat pt-[11rem] sm:items-start sm:bg-about-hero-tablet">
+      <div className="mb-[8.7rem] flex max-w-[40rem] flex-col px-[2.4rem] text-center sm:p-0 sm:pl-[5.8rem] sm:text-left">
         <h1 className="mb-[2.4rem] font-fraunces text-[2.8rem] font-black text-white">
           About Us
         </h1>
-        <p className="font-barlow text-[1.5rem] text-[#FEFCF7] opacity-80">
+        <p className="font-barlow text-[1.5rem] text-[#FEFCF7] opacity-80 sm:leading-[2.5rem]">
           Coffeeroasters began its journey of exotic discovery in 1999,
           highlighting stories of coffee from around the world. We have since
           been dedicated to bring the perfect cup - from bean to brew - in every
