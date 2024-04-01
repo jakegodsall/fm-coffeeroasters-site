ad26175827b9bb073903d0caa6b5ab9bf794b089
Jake Godsall <91415918+jakegodsall@users.noreply.github.com>
2024-04-01 14:55:45 +0100

    styled SubscribeSummary component of subscribe page

--- src/app/subscribe/components/SubscribeForm/SubscribeSummary.jsx
+++ src/app/subscribe/components/SubscribeForm/SubscribeSummary.jsx
@@ -6,18 +6,18 @@ export default function SubscribeSummary({ answers }) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="flex flex-col bg-[#293039] p-[2.4rem]"
+      className="mb-[6rem] flex flex-col rounded-[1rem] bg-[#293039] p-[2.4rem]"
     >
       <h5 className="mb-[0.8rem] font-barlow text-[1.6rem] uppercase text-off-white opacity-50">
         Order Summary
       </h5>
       <p className="font-fraunces text-[2.4rem] leading-[4rem] text-off-white">
-        I drink my coffee as{" "}
+        &ldquo;I drink my coffee as{" "}
         <span className="text-[#0E8784]">{answers[0]}</span>, with a{" "}
         <span className="text-[#0E8784]">{answers[1]}</span> type of bean.{" "}
         <span>{answers[2]}</span> ground ala{" "}
         <span className="text-[#0E8784]">{answers[3]}</span>, sent to me{" "}
-        <span className="text-[#0E8784]">{answers[4]}</span>.
+        <span className="text-[#0E8784]">{answers[4]}</span>.&rdquo;
       </p>
     </motion.div>
   );
