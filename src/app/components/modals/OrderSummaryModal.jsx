3a8538127fc4d7a84da70abbe76c972c4087ad77
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-04 16:23:20 +0100

    fixed the checkout button in desktop layout

--- src/app/components/modals/OrderSummaryModal.jsx
+++ src/app/components/modals/OrderSummaryModal.jsx
@@ -42,6 +42,15 @@ export default function OrderSummaryModal({
         >
           Checkout - $14.00 / mo
         </p>
+        <div className="mb-[5.6rem] hidden items-center gap-[2.5rem] sm:flex">
+          <p className="font-fraunces text-[3.2rem]">$14.00 / m</p>
+          <p
+            onClick={openThankYouModal}
+            className="cursor-pointer rounded-[0.6rem] bg-[#0e8784] px-[6.4rem] py-[1.6rem] font-fraunces text-[1.8rem] text-off-white transition-all duration-300 hover:bg-[#66d2cf]"
+          >
+            Checkout
+          </p>
+        </div>
       </motion.div>
     </>
   );
